1b1fe30c314d5727de802df6a749a6d626ca2b77
Anastasia Chen <anastasia@Anastasias-MacBook-Air.local>
2021-03-10 01:45:11 +1100

    Add homepage view, routes, API

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 module.exports = {
   port: process.env.PORT,
+
   pgport: process.env.POSTGRES_PORT,
   pguser: process.env.POSTGRES_USERNAME,
   pgdatabase: process.env.POSTGRES_DATABASE,
   pgpassword: process.env.POSTGRES_PASSWORD,
+
+  api_key: process.env.API_KEY
 };
 
 // This is how my .env looks like:
