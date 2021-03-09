26b7e2f6ae9782f467d5d09be37dda41c7759fca
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-03-09 11:51:35 +1100

    Updates modal to work with page html, separates password toggle functions

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    function openModal() {
-        $('.modal').removeClass('hidden');
-    }
+    $('#close-modal').click(modalToggle);
+    $('#open-modal').click(modalToggle);
+    $('#toggle-signup').click(toggleSignUp);
+    $('.toggle-login').click(toggleLogIn);
+    $('#forgotten-password').click(toggleForgottenPassword);
 
-    function closeModal() {
-        $('.modal').addClass('hidden');
-    }
+    $('#login-password + .password-toggle').click(passwordToggle1);
+    $('#signup-password + .password-toggle').click(passwordToggle2);
+    $('#confirm-password + .password-toggle').click(passwordToggle3);
 
-    function passwordToggle() {
-        const password = document.querySelectorAll('.password')
-        const togglePassword = document.querySelectorAll('.password + i.fas')
-        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    
 
-        password.setAttribute('type', type);
-        togglePassword.classList.toggle('fa-eye-slash')
+    function modalToggle() {
+        const modal = document.querySelector('.modal')
+        modal.classList.toggle('active')
     }
 
     function toggleForgottenPassword() {
@@ -33,5 +33,32 @@ document.addEventListener("DOMContentLoaded", () => {
         $('.forgotten-password').addClass('hidden');
         $('.login').removeClass('hidden')
         $('.signup').addClass('hidden')
+    }
+
+    function passwordToggle1() {
+        const password = document.querySelector('#login-password')
+        const togglePassword = document.querySelector('#login-password + i.fas')
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+
+        password.setAttribute('type', type);
+        togglePassword.classList.toggle('fa-eye-slash')
+    }
+
+    function passwordToggle2() {
+        const password = document.querySelector('#signup-password')
+        const togglePassword = document.querySelector('#signup-password + i.fas')
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+
+        password.setAttribute('type', type);
+        togglePassword.classList.toggle('fa-eye-slash')
+    }
+
+    function passwordToggle3() {
+        const password = document.querySelector('#confirm-password')
+        const togglePassword = document.querySelector('#confirm-password + i.fas')
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+
+        password.setAttribute('type', type);
+        togglePassword.classList.toggle('fa-eye-slash')
     }
 })
