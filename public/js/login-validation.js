03e04a6bd4baea8bf7b9d92c87bcc481ad42b65c
Amelia <limamelia.yx@gmail.com>
2021-03-15 23:15:13 +1100

    Minor changes to width for modal  and hero movie background

---
+++ public/js/login-validation.js
@@ -0,0 +1,31 @@
+const loginForm = document.getElementById("login-form");
+const emailOne = document.getElementById("login-email");
+const passwordOne = document.getElementById("login-password");
+
+
+loginForm.addEventListener("submit", e => {
+
+  if (!checkLoginInput())
+    e.preventDefault();
+
+});
+
+function checkLoginInput() {
+  const emailValueOne = emailOne.value;
+  const passwordValueOne = passwordOne.value;
+
+  if (emailValueOne === "") {
+    setErrorFor(emailOne, "Email address cannot be blank");
+  } else if (!isEmail1(emailValueOne)) {
+    setErrorFor(emailOne, "Email is not a valid address");
+  } else {
+    setSuccessFor(emailOne);
+  }
+
+  if (passwordValueOne === "") {
+    setErrorFor(passwordOne, "Password cannot be blank");
+  } else {
+    setSuccessFor(passwordOne);
+  }
+  return true;
+}
